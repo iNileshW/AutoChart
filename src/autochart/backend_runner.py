435906f00ba33ac5609f23537070e1bab78f1f88
iNileshW <33792419+iNileshW@users.noreply.github.com>
2026-08-05 11:49:25 +0100

    Parameters changes

--- src/autochart/backend_runner.py
+++ src/autochart/backend_runner.py
@@ -3,3 +3,9 @@ import uvicorn
 
 def main() -> None:
     uvicorn.run("autochart.backend.main:app", host="0.0.0.0", port=8000, reload=True)
+
+def matchData(chartNumber: str) -> bool:
+    if "match" in chartNumber:
+        return True
+    else:
+        return False
